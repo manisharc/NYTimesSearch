74d1f02ab0938ed77da34d46575c808f7fcb8bab
Roy Choudhury <chmanish@f45c898c8a4d.ant.amazon.com>
2016-10-21 11:04:05 -0700

    First commit from video walkthrough

---
+++ app/src/main/java/com/example/chmanish/nytimessearch/activities/ArticleActivity.java
@@ -0,0 +1,34 @@
+package com.example.chmanish.nytimessearch.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.chmanish.nytimessearch.R;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String url = getIntent().getStringExtra("url");
+        WebView webView = (WebView) findViewById(R.id.wvArticle);
+
+        webView.setWebViewClient(new WebViewClient() {
+                                     @Override
+                                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                         view.loadUrl(url);
+                                         return true;
+                                     }
+                                 });
+        webView.loadUrl(url);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/chmanish/nytimessearch/activities/SearchActivity.java
@@ -0,0 +1,140 @@
+package com.example.chmanish.nytimessearch.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import com.example.chmanish.nytimessearch.R;
+import com.example.chmanish.nytimessearch.adapters.ArticleArrayAdapter;
+import com.example.chmanish.nytimessearch.models.Article;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
+
+    ArrayList<Article> articles;
+    ArticleArrayAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupViews();
+
+    }
+
+    public void setupViews(){
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        articles = new ArrayList<>();
+        adapter = new ArticleArrayAdapter(this, articles);
+        gvResults.setAdapter(adapter);
+
+        //hook up listener for grid click
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //create an intent to display the article
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+                //get the article to display
+                Article article = articles.get(position);
+                //pass in that article into intent
+                i.putExtra("url", article.getWebUrl());
+                //launch the activity
+                startActivity(i);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+        //Toast.makeText(this, "Searching for" + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key", "0615376bbf4c4801a036cccbc32d58f0");
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray articleJsonResults = null;
+
+                try{
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
